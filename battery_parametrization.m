166978826725ee919e7dea24ee070b9391f44fc1
Marco Ragone <mragon2@uic.edu>
2021-02-19 09:25:58 -0600

    Add files via upload

---
+++ battery_parametrization.m
@@ -0,0 +1,13 @@
+cd ..
+cd ..
+cd ..
+T=20;
+% battery parametrization data from comsol
+cd ('Vitaliy_data/datasheet_steps_C_NMC_TeslaS');
+
+all_steps;
+
+% come back to the current folder
+cd ..
+cd ..
+cd ('simulink/C_NMC_TeslaS/FTP75');
